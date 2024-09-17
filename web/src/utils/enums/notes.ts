d0b0f4e1a4602787c6227e14c45a143d635757d8
Pedro <71912007+pedro-augbs@users.noreply.github.com>
2024-09-17 16:19:18 -0300

    init: base configs

---
+++ web/src/utils/enums/notes.ts
@@ -0,0 +1,14 @@
+class Note {
+	private note: string;
+	private type: string;
+
+	constructor(note: string, type: string) {
+		this.note = note;
+		this.type = type;
+	}
+
+	scale() {
+		if (this.type === "major") {
+		}
+	}
+}
